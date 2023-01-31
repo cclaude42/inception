634228cd21408b30f8b0eaf8ef642b7199a5d18d
César Claude <cesarclaudepro@gmail.com>
2023-01-31 21:56:48 +0100

    :construction: laying down a foundation

--- requirements/mariadb/setup.sh
+++ requirements/mariadb/setup.sh
@@ -7,3 +7,5 @@ echo "CREATE DATABASE wordpress;" | mysql
 echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASS';" | mysql
 echo "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%';" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
+
+tail -f /dev/null # HACKY ??
--- requirements/wordpress/setup.sh
+++ requirements/wordpress/setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-sed -i s/ENV_GET_USER/$MYSQL_USER/g /var/www/wordpress/wp-config.php
-sed -i s/ENV_GET_PASS/$MYSQL_PASS/g /var/www/wordpress/wp-config.php
+
+cp /wordpress/* /var/www/
+
+sed -i s/ENV_GET_USER/$MYSQL_USER/g /var/www/wp-config.php
+sed -i s/ENV_GET_PASS/$MYSQL_PASS/g /var/www/wp-config.php
+
+tail -f /dev/null # HACKY ??
