0b15df7f62b6f58579dbcdeecb2ade74ea823f1c
César Claude <cesarclaudepro@gmail.com>
2023-02-02 00:32:46 +0100

    :construction: working on mariadb connection

--- requirements/mariadb/setup.sh
+++ requirements/mariadb/setup.sh
@@ -8,4 +8,6 @@ echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASS';" | mysql
 echo "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%';" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
 
+# sed -i s/"socket = \/run\/mysqld\/mysqld.sock"/"port = 3306"/g /etc/mysql/mariadb.cnf ??
+
 /bin/bash
--- requirements/wordpress/wp-config.php
+++ requirements/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'ENV_GET_USER' );
 define( 'DB_PASSWORD', 'ENV_GET_PASS' );
 
 /** Database hostname */
-define( 'DB_HOST', 'mariadb' );
+define( 'DB_HOST', 'mariadb:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
