8c4146f854baf7b614b40c39b7970365e14d5dc1
Cesar Claude <cesarclaudepro@gmail.com>
2023-02-02 20:20:43 +0000

    :sparkles: got 443 to work, website seems to work

--- requirements/mariadb/setup.sh
+++ requirements/mariadb/setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install MariaDB
-apt -qq install -y mariadb-server > /dev/null
+apt -qq update -y && apt -qq install -y mariadb-server > /dev/null
 
 # Configure MariaDB
 sed -i s/"127.0.0.1"/"*"/g /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i s/"socket ="/"# "/g /etc/mysql/mariadb.cnf
+sed -i s/"socket ="/"#"/g /etc/mysql/mariadb.cnf
 
 # Create DB and USER
 service mariadb start
-sleep 5
+sleep 3
 mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASS';"
 mysql -e "GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%';"
